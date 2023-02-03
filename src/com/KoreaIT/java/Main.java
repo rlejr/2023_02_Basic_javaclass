1f325e05dda14157bf0e92a01a3f9cf0ced3815a
rlejr <rlejr159753@gmail.com>
2023-02-03 14:19:08 +0900

    게시판 만들기 ver 2

---
+++ src/com/KoreaIT/java/Main.java
@@ -0,0 +1,84 @@
+package com.KoreaIT.java;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("==프로그램 시작==");
+
+		Scanner sc = new Scanner(System.in);
+
+		int lastArticleId = 0;
+
+		List<Article> articles = new ArrayList<>();
+
+		while (true) {
+
+			System.out.printf("명령어 ) ");
+			String command = sc.nextLine().trim();
+
+			if (command.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
+			if (command.equals("system exit")) {
+				break;
+			}
+
+			if (command.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+					continue;
+				}
+				System.out.println("번호    /     제목    ");
+				String tempTitle = null;
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					if (article.title.length() > 4) {
+						tempTitle = article.title.substring(0, 4);
+						System.out.printf("%d	/	%s\n", article.id, tempTitle + "...");
+						continue;
+					}
+
+					System.out.printf("%d	/	%s\n", article.id, article.title);
+				}
+			} else if (command.equals("article write")) {
+				int id = lastArticleId + 1;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.printf("%d번 글이 생성 되었습니다\n", id);
+				lastArticleId++;
+			}
+
+			else {
+				System.out.println("존재하지 않는 명령어입니다");
+			}
+
+		}
+
+		System.out.println("==프로그램 끝==");
+
+		sc.close();
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+}
