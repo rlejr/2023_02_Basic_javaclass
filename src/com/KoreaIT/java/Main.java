525a43fbfcf53042e28a3cb9f6baedad0fb87c49
rlejr <rlejr159753@gmail.com>
2023-02-03 15:53:28 +0900

    게시판 날짜 정보저장,글 지우기, 글 미리보기

--- src/com/KoreaIT/java/Main.java
+++ src/com/KoreaIT/java/Main.java
@@ -1,5 +1,7 @@
 package com.KoreaIT.java;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -51,12 +53,55 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
+				LocalDateTime now = LocalDateTime.now();
 
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, title, body, now);
 				articles.add(article);
 
 				System.out.printf("%d번 글이 생성 되었습니다\n", id);
 				lastArticleId++;
+			} else if (command.startsWith("article detail ")) {
+				String[] commandBits = command.split(" ");
+
+				int id = Integer.parseInt(commandBits[2]);
+
+				Article foundArticle = null;
+
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				System.out.println(id + "번 글은 존재합니다");
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", foundArticle.formatedNow);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+			} else if (command.startsWith("article delete ")) {
+				String[] commandBits = command.split(" ");
+				int id = Integer.parseInt(commandBits[2]);
+				Article foundArticle = null;
+				for (int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					if (article.id == id) {
+						foundArticle = article;
+						articles.remove(i);
+						break;
+					}
+				}
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				System.out.println(id + "번 글을 삭제 되었습니다.");
+
 			}
 
 			else {
@@ -75,10 +120,12 @@ class Article {
 	int id;
 	String title;
 	String body;
+	String formatedNow;
 
-	Article(int id, String title, String body) {
+	Article(int id, String title, String body, LocalDateTime now) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
+		this.formatedNow = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
 	}
 }
